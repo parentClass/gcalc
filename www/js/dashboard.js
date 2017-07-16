0afd3d451eeb35fddfb3ee4b510929089437d800
parentClass <dimalantadaniel@gmail.com>
2017-07-16 14:44:12 +0800

    grade card added

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
+	$('.modal').modal();
+
+	$('.calculate-semester').on('click',function(){
+		$('.button-collapse').sideNav('hide');
+	});
 });
